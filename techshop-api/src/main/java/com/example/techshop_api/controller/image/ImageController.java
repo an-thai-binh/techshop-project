7f67f4398c548db3c5192cc0cc22deee85661f44
An Binh <21130287@st.hcmuaf.edu.vn>
2025-05-09 21:06:36 +0700

    them controller image

--- techshop-api/src/main/java/com/example/techshop_api/controller/image/ImageController.java
+++ techshop-api/src/main/java/com/example/techshop_api/controller/image/ImageController.java
@@ -1,17 +1,19 @@
 package com.example.techshop_api.controller.image;
 
+import com.example.techshop_api.dto.request.image.ImageCreationRequest;
 import com.example.techshop_api.dto.response.ApiResponse;
 import com.example.techshop_api.dto.response.image.ImageResponse;
 import com.example.techshop_api.service.ImageService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -21,10 +23,30 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageController {
     ImageService imageService;
 
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<ImageResponse>>> index(
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam(defaultValue = "id") String sort,
+            @RequestParam(defaultValue = "desc") String direction
+    ) {
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Sort sortBy = Sort.by(sortDirection, sort);
+        Pageable pageable = PageRequest.of(page, size, sortBy);
+        ApiResponse<Page<ImageResponse>> apiResponse = imageService.index(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
+    @PostMapping("/url")
+    public ResponseEntity<ApiResponse<ImageResponse>> store(@RequestBody ImageCreationRequest request) {
+        ApiResponse<ImageResponse> apiResponse = imageService.store(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
     @PostMapping("/file")
     public ResponseEntity<ApiResponse<ImageResponse>> store(@RequestParam("file") MultipartFile file) {
         ApiResponse<ImageResponse> apiResponse = imageService.store(file);
-        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
     }
 }
 
