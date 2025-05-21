d2728ff9e3d416774d072ae872661ed433d738b5
YoungLIBit <danghuutrong2016@gmail.com>
2025-05-21 09:11:16 +0700

    add route auth

---
+++ techshop-app/src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className={'flex'}>
+      <h1>Đây là trang Đăng nhập</h1>
+    </div>
+  )
+}
---
+++ techshop-app/src/app/(auth)/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className={'flex'}>
+      <h1>Đây là trang đăng nhập</h1>
+    </div>
+  )
+}
