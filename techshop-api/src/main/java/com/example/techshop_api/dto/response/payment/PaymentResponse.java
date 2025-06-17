545bd9558a7032a16d1772671ce2fd72e897dd96
An Binh <21130287@st.hcmuaf.edu.vn>
2025-06-17 15:02:10 +0700

    them giao dien chi tiet don hang (admin), chinh sua PaymentDetailResponse

--- techshop-api/src/main/java/com/example/techshop_api/dto/response/payment/PaymentResponse.java
+++ techshop-api/src/main/java/com/example/techshop_api/dto/response/payment/PaymentResponse.java
@@ -11,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 public class PaymentResponse {
     Long id;
     Long orderId;
+    double amount;
     String paymentMethod;
     String paymentStatus;
     String paymentGateway;
